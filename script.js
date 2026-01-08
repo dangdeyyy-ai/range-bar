63719cff110fa4860143cfd8aabc0ed22a80f7d0
dangdeyyy <dangdeyyy@gmail.com>
2026-01-08 10:42:23 +0700

    update code

---
+++ script.js
@@ -0,0 +1,56 @@
+const sliderInput = document.querySelectorAll(".slider-box input"),
+    priceInput = document.querySelectorAll(".price-input-box input"),
+    fill = document.querySelector(".slider-fill"),
+    minDisplay = document.getElementById("minDisplay"),
+    maxDisplay = document.getElementById("maxDisplay");
+
+let priceGap = 1;
+
+const input = {
+    get min() {
+        return parseInt(sliderInput[0].value);
+    },
+    get max() {
+        return parseInt(sliderInput[1].value);
+    },
+};
+
+function updateDisplay() {
+    minDisplay.textContent = `$${input.min}`;
+    maxDisplay.textContent = `$${input.max}`;
+
+    priceInput[0].value = input.min;
+    priceInput[1].value = input.max;
+
+    const maxRange = sliderInput[0].max;
+    fill.style.left = (input.min / maxRange) * 100 + "%";
+    fill.style.right = 100 - (input.max / maxRange) * 100 + "%";
+}
+
+sliderInput.forEach((slide) => {
+    slide.addEventListener("input", (e) => {
+        if (input.max - input.min < priceGap) {
+            if (e.target.classList.contains("min-range")) {
+                sliderInput[0].value = input.max - priceGap;
+            } else {
+                sliderInput[1].value = input.min + priceGap;
+            }
+        }
+        updateDisplay();
+    });
+});
+
+priceInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        if (input.max - input.min >= priceGap && maxInput <= 5000) {
+            if (e.target.classList.contains("input-min")) {
+                sliderInput[0].value = input.min;
+            } else {
+                sliderInput[1].value = input.max;
+            }
+            updateDisplay();
+        }
+    });
+});
+
+updateDisplay();
